d25289473f767e7bfab6af950a7c0730b007524d
Ryan Jones <ryan.jones@siliconstudio.co.jp>
2019-03-01 18:02:43 +0900

    add VL node, some BVH starter logic

---
+++ physical/BVH.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+
+namespace physical
+{
+    public class BVH
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BVHNode
+        {
+            public BoundingBox AABB;
+            public int ChildA;
+            public int ChilbB;
+
+            public BVHNode(BoundingBox aabb, int childA, int childB)
+            {
+                AABB = aabb;
+                ChildA = childA;
+                ChilbB = childB;
+            }
+        }
+    }
+
+    public static class BVHBuilder
+    {
+        // Calculate world bounds from a list of primitives.
+        public static BoundingBox CalculateWorldBounds(IEnumerable<Primitive> primitives)
+        {
+            BoundingBox WorldAABB = new BoundingBox(Vector3.Zero, Vector3.Zero);
+
+            if (!primitives.Any())
+            {
+                return WorldAABB;
+            }
+
+            foreach (var primitive in primitives)
+            {
+                BoundingBox.Merge(WorldAABB, primitive.AABB);
+            }
+
+            return WorldAABB;
+        }
+
+        public static IEnumerable<BVH.BVHNode> BuildBVH(IEnumerable<Primitive> primitives, BoundingBox WorldBounds)
+        {
+            if (!primitives.Any() || WorldBounds.Minimum == Vector3.Zero && WorldBounds.Maximum == Vector3.Zero)
+            {
+                return new List<BVH.BVHNode>();
+            }
+        }
+    }
+}
--- physical/Types.cs
+++ physical/Types.cs
@@ -6,23 +6,22 @@ namespace physical
 {
     public enum PrimitiveType
     {
-        Sphere,
-        Box,
-        SDF
+        Sphere = 0,
+        Box = 1,
+        SDF = 2
     }
 
     public enum MaterialType
     {
-        Dielectric,
-        Metallic,
-        Emissive
+        Dielectric = 0,
+        Metallic = 1,
+        Emissive = 2
     }
 
     public enum LightType
     {
-        Point,
-        Spot,
-        Directional
+        Point = 0,
+        Spot = 1
     }
 
     [StructLayout(LayoutKind.Sequential)]
@@ -32,14 +31,39 @@ namespace physical
         public uint MaterialIndex;
         public Vector4 Params;
         public Matrix InverseTransform;
+        public BoundingBox AABB;
 
         public Primitive(PrimitiveType primitiveType, uint materialIndex, Vector4 primitiveParams,
-            Matrix inverseTransform)
+            Matrix transform)
         {
             PrimitiveType = primitiveType;
             MaterialIndex = materialIndex;
             Params = primitiveParams;
-            InverseTransform = inverseTransform;
+            InverseTransform = transform;
+
+            // All transforms going into the raytracer need to be inverse for ray calculation
+            InverseTransform.Invert();
+
+            Vector3 pos = transform.TranslationVector;
+
+            switch (primitiveType)
+            {
+                case PrimitiveType.Sphere:
+                    float radius = primitiveParams.X;
+                    BoundingSphere bSphere = new BoundingSphere(pos, radius);
+                    AABB = BoundingBox.FromSphere(bSphere);
+                    break;
+
+                case PrimitiveType.Box:
+                    Vector3 size = new Vector3(primitiveParams.X, primitiveParams.Y, primitiveParams.Z);
+                    AABB = new BoundingBox(pos - size / 2.0f, pos + size / 2.0f);
+                    break;
+
+                default:
+                    Vector3 defaultSize = new Vector3(1.0f, 1.0f, 1.0f);
+                    AABB = new BoundingBox(pos - defaultSize / 2.0f, pos + defaultSize / 2.0f);
+                    break;
+            }
         }
     }
 
